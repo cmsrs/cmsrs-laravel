ced3c65e489a4242a89bf045cdf2b11c8fe0257d
Robert <robert.szczepanski@cmsrs.pl>
2026-09-23 19:24:24 +0200

    ChangeAdminPassTest

---
+++ tests/Feature/Console/Cmsrs/ChangeAdminPassTest.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Feature\Console\Cmsrs;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ChangeAdminPassTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_admin_password_is_changed(): void
+    {
+        $admin = User::create([
+            'name' => 'admin',
+            'email' => 'adm@cmsrs.pl',
+            'role' => User::$role_dict['admin'],
+            'password' => 'old-password',
+        ]);
+
+        $oldPassword = $admin->password;
+
+        $this->artisan('cmsrs:change-admin-pass', [
+            'pass' => 'new-password',
+        ])
+            ->assertExitCode(0);
+
+        $admin->refresh();
+
+        $this->assertNotEquals(
+            $oldPassword,
+            $admin->password
+        );
+    }
+
+}
